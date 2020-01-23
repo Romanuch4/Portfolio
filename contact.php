e54cc8c8f903081faf87638a2a0b26c87d8f58c0
Roman <pomankrentovsky@gmail.com>
2020-01-23 16:49:34 +0200

    тест 4

--- contact.php
+++ contact.php
@@ -1,3 +1,11 @@
 <?php 
-echo $_SERVER['SERVER_ADDR'];
+
+$recepient = "pomankrentovsky@gmail.com";
+$sitename = "My portfolio";
+
+$email = trim($_POST["email"]);
+$message = "Email: $email";
+
+$pagetitle = "Новый email с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient")
 ?>
--- js/contact-form.js
+++ js/contact-form.js
@@ -3,7 +3,7 @@
 
   const ajaxGet = function() {
     const request = new XMLHttpRequest();
-    request.open('POST', 'contact.php');
+    request.open('GET', 'contact.php');
     request.send();
     request.onreadystatechange = function() {
       if(request.readyState == 4 && request.status == 200) {
