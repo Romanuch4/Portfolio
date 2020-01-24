2fe48a3128697c672dbd539581ee53b1c650ee8a
Roman <pomankrentovsky@gmail.com>
2020-01-24 16:12:47 +0200

    тест 5

--- js/contact-form.js
+++ js/contact-form.js
@@ -4,8 +4,8 @@
   const ajaxGet = function (evt) {
     evt.preventDefault();
     const request = new XMLHttpRequest();
-    request.open('GET', 'contact.php');
-    request.send();
+    request.open('GET', '../contact.php', true);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-url');
     request.onreadystatechange = function() {
       if(request.readyState == 4 && request.status == 200) {
         console.log('1234567890');
@@ -15,6 +15,8 @@
        /*  window.location.href = 'https://romanuch4.github.io/Portfolio/'; */
       }
     }
+
+    request.send();
   }
 
   button.addEventListener('click', evt => ajaxGet(evt));
